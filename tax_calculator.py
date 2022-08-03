8561cb80e6140a1339dda4783892bc5c74ad6140
Harman-19 <harmanpreet@incites.com>
2022-08-03 16:00:21 +1000

    -Implemented get_year_prompt function -Updated tax rates for year 2021-2022

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,1 +1,26 @@
+import pandas as pd
+import re
 
+
+def get_year_prompt() -> str:
+    """
+    Prompts user for year to calculate the tax for.
+
+    Returns:
+        str: year (e.g.2020-2021)
+
+    """
+    year = input("Please enter the income year (eg: 2020-2021): ")
+    year = year.strip()
+
+    # Keep prompting the user for input until a valid year is entered
+    if not re.fullmatch(r'\d{4}-\d{4}', year):
+        print("Invalid Input. Kindly try again.")
+        return get_year_prompt()
+
+    # Check whether the tax year is in the database and quit if not
+    if year not in pd.ExcelFile('tax_rates.xlsx').sheet_names:
+        print("Tax information not available for the given year.")
+        exit()
+
+    return year
